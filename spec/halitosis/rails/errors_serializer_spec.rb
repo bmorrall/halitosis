975667176fd6d9f1a5bbd310010f98e064c62675
Ben Morrall <bemo56@hotmail.com>
2026-05-22 15:21:24 +1000

    Allow source_pointer to be overriden with builder

--- spec/halitosis/rails/errors_serializer_spec.rb
+++ spec/halitosis/rails/errors_serializer_spec.rb
@@ -7,6 +7,65 @@ RSpec.describe Halitosis::ErrorsSerializer, :rails do
 
   let(:error_class) { Struct.new(:type, :full_message, :attribute, keyword_init: true) }
 
+  describe ".build" do
+    let(:errors) { [error_class.new(type: :blank, full_message: "Title can't be blank", attribute: :title)] }
+
+    it "returns an ErrorsSerializer instance" do
+      result = described_class.build(errors, param: "article") { title { "Validation Error" } }
+
+      expect(result).to be_a(described_class)
+    end
+
+    it "raises ArgumentError when called without a block" do
+      expect { described_class.build(errors, param: "article") }.to raise_error(ArgumentError, /requires a block/)
+    end
+
+    it "accepts an empty block and falls back to ErrorSerializer defaults" do
+      result = described_class.build(errors, param: "article") {}.as_json
+
+      error_object = result["errors"].first
+      expect(error_object["code"]).to eq("title_blank")
+      expect(error_object["detail"]).to eq("Title can't be blank")
+    end
+
+    it "inherits code and detail from ErrorSerializer" do
+      result = described_class.build(errors, param: "article") {
+        title { "Validation Error" }
+      }.as_json
+
+      error_object = result["errors"].first
+      expect(error_object["code"]).to eq("title_blank")
+      expect(error_object["detail"]).to eq("Title can't be blank")
+    end
+
+    it "renders one entry per error" do
+      two_errors = [
+        error_class.new(type: :blank, full_message: "Title can't be blank", attribute: :title),
+        error_class.new(type: :too_short, full_message: "Body is too short", attribute: :body)
+      ]
+
+      result = described_class.build(two_errors, param: "article") { title { "Error" } }.as_json
+
+      expect(result["errors"]).to have_attributes(size: 2)
+    end
+
+    it "overrides the default source pointer with source_parameter" do
+      result = described_class.build(errors, param: "article") {
+        source_parameter { "filter[status]" }
+      }.as_json
+
+      expect(result["errors"].first["source"]).to eq("parameter" => "filter[status]")
+    end
+
+    it "overrides the default source pointer with a custom source_pointer" do
+      result = described_class.build(errors, param: "article") {
+        source_pointer { "/data/relationships/tags" }
+      }.as_json
+
+      expect(result["errors"].first["source"]).to eq("pointer" => "/data/relationships/tags")
+    end
+  end
+
   describe "#as_json" do
     context "with a field error" do
       let(:errors) { [error_class.new(type: :blank, full_message: "Title can't be blank", attribute: :title)] }
